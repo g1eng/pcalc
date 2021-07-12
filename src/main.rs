7d757d46fc2642667ba2b21aab64ce65a2064811
Suzume Nomura <suzume315@g00.g1e.org>
2021-07-12 20:34:40 +0900

    experimental option parsing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use structopt::StructOpt;
+//! pcalc - A high-speed primary calculator
+//!
+//!
 
-#[derive(StructOpt)]
-struct Cli {
-    capacity: usize,
-}
+
+use structopt::clap::{App, Arg};
+use std::str::FromStr;
 
 fn check_prime(testant: u64, vec: &[u64]) -> bool {
     let mut i = 0;
@@ -21,17 +22,77 @@ fn check_prime(testant: u64, vec: &[u64]) -> bool {
 }
 
 fn main() {
-    let arg = Cli::from_args();
+    let arg = App::new("pcalc")
+        .version("0.3.0")
+        .author("Suzume Nomura")
+        .arg(
+            Arg::with_name("capacity")
+            .value_name("LIMIT")
+            .index(1)
+            .help("calculation limit for primary number sequence")
+            .required(true)
+            )
+        .arg(
+            Arg::with_name("verbose")
+            .long("verbose")
+            .help("print all result for the calculation")
+            .required(false)
+            )
+        .arg(
+            Arg::with_name("get")
+            .short("g")
+            .long("get")
+            .help("get number with a sequence")
+            .required(false)
+            )
+        .arg(
+            Arg::with_name("pop")
+            .short("p")
+            .long("pop")
+            .help("popup last primary of the calculation")
+            .required(false)
+            )
+        .arg(
+            Arg::with_name("hex")
+            .short("H")
+            .help("print value as a hex number")
+            .required(false)
+            )
+        .get_matches();
+
+    let verbose = arg.is_present("verbose");
+    let with_hex = arg.is_present("hex");
+    let with_pop = arg.is_present("pop");
+    let capacity = match arg.value_of("capacity"){
+        Some(x) => usize::from_str(x).expect("invalid capacity number!"),
+        None => {
+            println!("invalid capacity number, abort!");
+            return
+        }
+    };
+
+
     let mut pr: Vec<u64> = vec![2];
     let mut i: u64 = 3;
     loop {
         if check_prime(i, &pr) {
             pr.push(i);
         }
-        if pr.len() >= arg.capacity {
+        if pr.len() >= capacity {
             break;
         }
         i += 2;
     }
-    println!("{:?}", pr);
+
+    if verbose {
+        println!("{:?}", pr);
+    }
+    if with_pop {
+        let last = pr[pr.len() - 1];
+        if with_hex {
+            println!("{:?}", format!("{:#x}", last));
+        } else {
+            println!("{}", last);
+        }
+    }
 }
